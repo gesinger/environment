7984f2ab54952a343d94890d69e90d635eb06055
Garrett Singer <gesinger@gmail.com>
2014-11-22 21:53:38 -0800

    Add pathogen installation as well as vim-sensible and the jellbeans colorscheme to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+HOMEDIR=/home/vagrant
+
 apt-get update
 apt-get install -y docker
 apt-get install -y git
+
+# VIM
+
+mkdir -p $HOMEDIR/.vim/autoload $HOMEDIR/.vim/bundle && \
+curl -LSso $HOMEDIR/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cd $HOMEDIR/.vim/bundle && \
+git clone git://github.com/tpope/vim-sensible.git
+
+cd $HOMEDIR/.vim/bundle && \
+git clone git://github.com/nanotech/jellybeans.vim.git
