ee78a0acc140018e3b4369ffbdfee9a16a07bb09
Garrett Singer <gesinger@gmail.com>
2015-03-21 18:47:55 -0400

    Add script for installing node (iojs)

---
+++ programming_languages/node.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Note the version may have to be updated
+curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | bash
+# Must be run after shell restart
+bash
+nvm install iojs
