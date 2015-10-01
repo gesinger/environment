332fc4235f6099900262e93b1a6ae7b333aea9e8
Garrett Singer <gesinger@gmail.com>
2015-09-30 21:32:31 -0700

    Minor cleanup in physical bootstrap

--- bootstrap_physical.sh
+++ bootstrap_physical.sh
@@ -7,8 +7,6 @@ sudo apt-get install -y xinit
 sudo apt-get install -y i3
 sudo apt-get install -y xterm
 sudo apt-get install -y x11-xserver-utils
-# TODO: determine if necessary
-# sudo apt-get install ncurses...
 sudo apt-get install -y firefox
 sudo apt-get install -y wicd-curses
 sudo apt-get install -y powertop
@@ -17,6 +15,7 @@ sudo apt-get install -y pm-utils
 sudo apt-get install -y xclip
 sudo apt-get install -y silversearcher-ag
 sudo apt-get install -y argyll
+sudo apt-get install -y roboto
 
 if [ ! -e /usr/share/fonts/opentype/firamono ]; then
   sudo mkdir -p /usr/share/fonts/opentype/firamono
@@ -26,8 +25,6 @@ if [ ! -e /usr/share/fonts/opentype/firamono ]; then
   sudo wget http://github.com/mozilla/Fira/raw/master/otf/FiraMono-Regular.otf && \
   cd -
 fi
-
-sudo apt-get install -y roboto
 
 sudo add-apt-repository ppa:no1wantdthisname/ppa
 sudo apt-get update
