f16ad4918512d0309ce596d2ebdc9484c7cf4790
Garrett Singer <gesinger@gmail.com>
2015-10-15 19:00:25 -0700

    Add optional install for exdict

---
+++ optional/exdict.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ ! -d ~/repos/external/exdict ]; then
+  git clone http://github.com/gesinger/exdict ~/repos/external/exdict
+  cd ~/repos/external/exdict
+  sh generate_webster_1913_csv.sh
+  printf "#!/bin/bash\npython ~/repos/external/exdict/exdict.py $1" > ~/bin/exdict
+  chmod +x ~/bin/exdict
+  cd -
+fi
