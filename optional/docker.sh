89ee078fdf69541884dbb9ede4e9cb1cfe21a7f5
Garrett Singer <gesinger@gmail.com>
2015-12-06 14:20:52 -0800

    Make bin scripts executable for docker install

--- optional/docker.sh
+++ optional/docker.sh
@@ -5,4 +5,6 @@ sudo curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker garrett
 
 printf "#!/bin/bash\ndocker rm $(docker ps -a -q)" > ~/bin/docker_remove_stopped_containers
+chmod +x ~/bin/docker_remove_stopped_containers
 printf "#!/bin/bash\ndocker rmi $(docker images | grep \"^<none>\" | awk \"{print $3}\")" > ~/bin/docker_remove_untagged_images
+chmod +x ~/bin/docker_remove_untagged_images
