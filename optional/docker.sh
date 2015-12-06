546759254f2a5623097dc70521f39b2dbf434fb4
Garrett Singer <gesinger@gmail.com>
2015-12-06 14:19:32 -0800

    Add docker commands to bin in optional docker install

--- optional/docker.sh
+++ optional/docker.sh
@@ -3,3 +3,6 @@
 sudo apt-get install -y curl
 sudo curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker garrett
+
+printf "#!/bin/bash\ndocker rm $(docker ps -a -q)" > ~/bin/docker_remove_stopped_containers
+printf "#!/bin/bash\ndocker rmi $(docker images | grep \"^<none>\" | awk \"{print $3}\")" > ~/bin/docker_remove_untagged_images
