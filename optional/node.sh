cef4e11aaa0b1be3ad2d894c63fea6db2964d9ce
Garrett Singer <gesinger@gmail.com>
2016-04-26 09:27:57 -0400

    Rename optional node install to nvm, update version, and don't install a version by default

--- optional/node.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Note the version may have to be updated
-curl https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | bash
-# Must be run after shell restart
-bash
-nvm install iojs
---
+++ optional/nvm.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Note the version may have to be updated
+curl https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
