855d3d0824b3d786dd26fe10e41dc51efb52188f
Garrett Singer <gesinger@gmail.com>
2016-04-26 04:24:07 -0700

    Don't change directory when git cloning vim plugins

--- bootstrap_user.sh
+++ bootstrap_user.sh
@@ -17,28 +17,25 @@ if [ ! -e ~/.vim/autoload/pathogen.vim ]; then
 fi
 
 if [ ! -d ~/.vim/bundle/vim-sensible ]; then
-  cd ~/.vim/bundle && \
-  git clone git://github.com/tpope/vim-sensible.git
+  git clone git://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible
 fi
 
 if [ ! -d ~/.vim/bundle/vim-distinguished ]; then
-  cd ~/.vim/bundle && \
-  git clone git://github.com/Lokaltog/vim-distinguished.git
+  git clone git://github.com/Lokaltog/vim-distinguished.git \
+    ~/.vim/bundle/vim-distinguished
 fi
 
 if [ ! -d ~/.vim/bundle/goyo.vim ]; then
-  cd ~/.vim/bundle && \
-  git clone git://github.com/junegunn/goyo.vim.git
+  git clone git://github.com/junegunn/goyo.vim.git ~/.vim/bundle/goyo.vim
 fi
 
 if [ ! -d ~/.vim/bundle/limelight.vim ]; then
-  cd ~/.vim/bundle && \
-  git clone git://github.com/junegunn/limelight.vim.git
+  git clone git://github.com/junegunn/limelight.vim.git \
+    ~/.vim/bundle/limelight.vim
 fi
 
 if [ ! -d ~/.vim/bundle/vim-airline ]; then
-  cd ~/.vim/bundle && \
-  git clone git://github.com/bling/vim-airline.git
+  git clone git://github.com/bling/vim-airline.git ~/.vim/bundle/vim-airline
 fi
 
 if [ ! -d ~/.vim/bundle/vimproc.vim ]; then
